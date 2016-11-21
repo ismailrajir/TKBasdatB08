0a2f81a95c5cc88dafc848ab0d81612d7663233d
JohannesAnugerah <johannes.bin419@gmail.com>
2016-11-21 21:10:04 +0700

    Add files via upload

---
+++ DOSEN.sql
@@ -0,0 +1,58 @@
+CREATE TABLE DOSEN (
+    `NIP` INT,
+    `nama` VARCHAR(17) CHARACTER SET utf8,
+    `username` VARCHAR(12) CHARACTER SET utf8,
+    `password` VARCHAR(12) CHARACTER SET utf8,
+    `email` VARCHAR(32) CHARACTER SET utf8,
+    `institusi` VARCHAR(58) CHARACTER SET utf8
+);
+INSERT INTO DOSEN VALUES (222222222,'Budi A','budi1','budi1','budi1@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222223,'Budi B','budi2','budi2','budi2@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222224,'Budi C','budi3','budi3','budi3@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222225,'Budi D','budi4','budi4','budi4@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222226,'Budi E','budi5','budi5','budi5@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222227,'Budi F','budi6','budi6','budi6@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222228,'Budi G','budi7','budi7','budi7@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222229,'Budi H','budi8','budi8','budi8@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222230,'Budi I','budi9','budi9','budi9@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222231,'Budi J','budi10','budi10','budi10@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222232,'Budi K','budi11','budi11','budi11@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222233,'Budi L','budi12','budi12','budi12@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222234,'Budi M','budi13','budi13','budi13@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222235,'Budi O','budi14','budi14','budi14@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222236,'Budi P','budi15','budi15','budi15@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222237,'Budi Q','budi16','budi16','budi16@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222238,'Budi R','budi17','budi17','budi17@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222239,'Budi S','budi18','budi18','budi18@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222240,'Budi T','budi19','budi19','budi19@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (222222241,'Budi U','budi20','budi20','budi20@gmail.com','Universitas Indonesia');
+INSERT INTO DOSEN VALUES (917141375,'Richard Bryant','rbryant0','Q7l0m5jn5cb','rbryant0@wikimedia.org','Universidad Metropolitana de Honduras');
+INSERT INTO DOSEN VALUES (340831963,'Katherine Brown','kbrown1','1EFFvlulHw','kbrown1@seesaa.net','Coastal Carolina University');
+INSERT INTO DOSEN VALUES (610259145,'Denise Jones','djones2','aYINH5hLJ0Y3','djones2@imdb.com','Universidad de Hermosillo');
+INSERT INTO DOSEN VALUES (573446902,'Justin Garrett','jgarrett3','uhZUWmD6MWcj','jgarrett3@yahoo.co.jp','Osaka Medical College');
+INSERT INTO DOSEN VALUES (803859305,'Jacqueline Wagner','jwagner4','R0oQfEGuAu','jwagner4@macromedia.com','Hardin-Simmons University');
+INSERT INTO DOSEN VALUES (791607366,'Randy Wheeler','rwheeler5','XUcUIL7z5','rwheeler5@360.cn','York College Nebraska');
+INSERT INTO DOSEN VALUES (722302430,'Sarah Elliott','selliott6','GEzcVu7WFk','selliott6@blinklist.com','University of Anbar');
+INSERT INTO DOSEN VALUES (608347411,'Earl Reynolds','ereynolds7','AO4TSV0cc3','ereynolds7@theatlantic.com','State University of New York Health Sience Centre Syracuse');
+INSERT INTO DOSEN VALUES (985685907,'Rebecca Franklin','rfranklin8','GXAhfuZh0','rfranklin8@ca.gov','Liaquat University of Medical & Health Sciences Jamshoro');
+INSERT INTO DOSEN VALUES (378642246,'Brandon Meyer','bmeyer9','LSZ654JSQaZ','bmeyer9@reddit.com','Fondazione Sacro Cuore');
+INSERT INTO DOSEN VALUES (627070601,'Ann Welch','awelcha','VYi4fvSng2RI','awelcha@yahoo.com','Westfield State College');
+INSERT INTO DOSEN VALUES (188860481,'Jonathan Flores','jfloresb','Jc6yVxPCPOhd','jfloresb@cnbc.com','Vesalius College');
+INSERT INTO DOSEN VALUES (404638653,'John Jones','jjonesc','LGYmuhsJ','jjonesc@symantec.com','Limestone College');
+INSERT INTO DOSEN VALUES (655641265,'Samuel Burke','sburked','l5zaFh','sburked@is.gd','Crichton College');
+INSERT INTO DOSEN VALUES (146907966,'Katherine Hunter','khuntere','mwz7rA','khuntere@statcounter.com','University of Berne');
+INSERT INTO DOSEN VALUES (971742901,'Douglas Hernandez','dhernandezf','0CshpRSgX6','dhernandezf@g.co','Mount Vernon Nazarene College');
+INSERT INTO DOSEN VALUES (725916126,'Earl Vasquez','evasquezg','zmA9zCKT4','evasquezg@over-blog.com','Baltimore International College');
+INSERT INTO DOSEN VALUES (323335664,'Richard Welch','rwelchh','DNN0sJT','rwelchh@java.com','Columbia Union College');
+INSERT INTO DOSEN VALUES (493821921,'Sean Nelson','snelsoni','b1Pk3Ht','snelsoni@naver.com','Higher Technological Institute');
+INSERT INTO DOSEN VALUES (124327566,'Bruce Morales','bmoralesj','PXedch0y','bmoralesj@homestead.com','Universitas Kristen Indonesia Tomohon');
+INSERT INTO DOSEN VALUES (671569858,'Judith Richardson','jrichardsonk','WKuAeBsHS3P','jrichardsonk@google.cn','Universidad de Celaya');
+INSERT INTO DOSEN VALUES (907683703,'Antonio Reynolds','areynoldsl','LffC2qxK','areynoldsl@senate.gov','Politeknik Pos Indonesia');
+INSERT INTO DOSEN VALUES (842944052,'Jesse Allen','jallenm','RONc9J','jallenm@smh.com.au','Institute of Teachers Education, Sarawak');
+INSERT INTO DOSEN VALUES (994006908,'Edward King','ekingn','pvLAW0Hq3sg','ekingn@businessinsider.com','City University of New York (CUNY) System');
+INSERT INTO DOSEN VALUES (287881464,'Judith Cooper','jcoopero','57OV9RE9fLH','jcoopero@devhub.com','First Global University to teaching Jainism');
+INSERT INTO DOSEN VALUES (325166710,'Janet Alvarez','jalvarezp','Qz0OXIleheCs','jalvarezp@miitbeian.gov.cn','Technical University in Kosice');
+INSERT INTO DOSEN VALUES (556332827,'John Graham','jgrahamq','g3kalDux','jgrahamq@newyorker.com','Free University of Bozen');
+INSERT INTO DOSEN VALUES (232499103,'Julia Frazier','jfrazierr','cWMvFeeWSYnj','jfrazierr@skype.com','Athlone Institute of Technology');
+INSERT INTO DOSEN VALUES (784142989,'Arthur Peterson','apetersons','YuFBvitiWe4','apetersons@webs.com','UCSI University');
+INSERT INTO DOSEN VALUES (944769142,'Jose Nichols','jnicholst','vz5LoKxtg','jnicholst@nationalgeographic.com','New College of California');
---
+++ MATA_KULIAH_SPESIAL.sql
@@ -0,0 +1,91 @@
+CREATE TABLE MATA_KULIAH_SPESIAL (
+    `IdMKS` INT,
+    `NPM` INT,
+    `Tahun` INT,
+    `Semester` INT,
+    `Judul` VARCHAR(73) CHARACTER SET utf8,
+    `IsSiapSidang` VARCHAR(5) CHARACTER SET utf8,
+    `PengumpulanHardCopy` VARCHAR(5) CHARACTER SET utf8,
+    `IjinMajuSidang` VARCHAR(5) CHARACTER SET utf8,
+    `IdJenisMKS` INT
+);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3333,111111111,2016,1,'Judul1','False','True','False',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3334,111111112,2016,2,'Judul2','False','True','False',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3335,111111113,2016,3,'Judul3','False','True','False',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3336,111111114,2016,1,'Judul4','False','False','True',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3337,111111115,2016,2,'Judul5','False','False','True',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3338,111111116,2016,3,'Judul6','True','False','True',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3339,111111117,2016,1,'Judul7','True','False','False',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3340,111111118,2016,2,'Judul8','True','False','False',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3341,111111119,2016,3,'Judul9','True','True','False',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3342,111111120,2016,1,'Judul10','True','True','False',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3343,111111121,2016,2,'Judul11','True','True','False',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3344,111111122,2016,3,'Judul12','True','True','True',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3345,111111123,2016,1,'Judul13','False','True','True',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3346,111111124,2016,2,'Judul14','False','True','True',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3347,111111125,2016,3,'Judul15','False','True','True',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3348,111111126,2016,1,'Judul16','False','False','True',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3349,111111127,2016,2,'Judul17','False','False','True',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3350,111111128,2016,3,'Judul18','True','False','True',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3351,111111129,2016,1,'Judul19','True','False','False',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3352,111111130,2016,2,'Judul20','True','False','False',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3353,111111131,2016,3,'Judul21','True','True','False',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3354,111111132,2016,1,'Judul22','True','True','False',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3355,111111133,2016,2,'Judul23','True','True','False',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3356,111111134,2016,3,'Judul24','True','True','True',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3357,111111135,2016,1,'Judul25','False','True','True',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3358,111111136,2016,2,'Judul26','False','True','True',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3359,111111137,2016,3,'Judul27','False','True','True',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3360,111111138,2016,1,'Judul28','False','False','True',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3361,111111139,2016,2,'Judul29','False','False','True',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3362,111111140,2016,3,'Judul30','True','False','True',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3363,111111141,2016,1,'donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin','True','False','False',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3364,111111142,2016,2,'gravida sem praesent id massa id nisl venenatis lacinia aenean','True','False','False',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3365,111111143,2016,3,'tellus semper interdum mauris ullamcorper purus sit','True','True','False',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3366,111111144,2016,1,'bibendum felis sed interdum venenatis turpis enim','True','True','False',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3367,111111145,2016,2,'maecenas pulvinar lobortis est phasellus sit','True','True','False',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3368,111111146,2016,3,'varius integer ac leo pellentesque ultrices mattis odio','True','True','True',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3369,111111147,2016,1,'eu nibh quisque id justo sit amet sapien dignissim','False','True','True',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3370,111111148,2016,2,'lacinia nisi venenatis tristique fusce','False','True','True',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3371,111111149,2016,3,'odio donec vitae nisi nam ultrices libero non mattis pulvinar','False','True','True',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3372,111111150,2016,1,'ac est lacinia nisi venenatis tristique fusce','False','False','True',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3373,596859185,2016,2,'at lorem integer tincidunt ante','False','False','True',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3374,901860343,2016,3,'nisi at nibh in hac habitasse platea dictumst aliquam augue','True','False','True',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3375,717576893,2016,1,'convallis tortor risus dapibus augue','True','False','False',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3376,671905104,2016,2,'dignissim vestibulum vestibulum ante ipsum primis','True','False','False',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3377,421978594,2016,3,'ut massa volutpat convallis morbi odio odio','True','True','False',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3378,515139958,2016,1,'dictumst morbi vestibulum velit id pretium iaculis diam','True','True','False',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3379,628993042,2016,2,'ante ipsum primis in faucibus orci luctus et','True','True','False',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3380,575880894,2016,3,'donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin','True','True','True',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3381,359670396,2016,1,'gravida sem praesent id massa id nisl venenatis lacinia aenean','False','True','True',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3382,464420869,2016,2,'tellus semper interdum mauris ullamcorper purus sit','False','True','True',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3383,517308172,2016,3,'bibendum felis sed interdum venenatis turpis enim','False','True','True',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3384,274542740,2016,1,'maecenas pulvinar lobortis est phasellus sit','False','False','True',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3385,504171890,2016,2,'varius integer ac leo pellentesque ultrices mattis odio','False','False','True',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3386,372488173,2016,3,'eu nibh quisque id justo sit amet sapien dignissim','True','False','True',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3387,437918915,2016,1,'lacinia nisi venenatis tristique fusce','True','False','False',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3388,338918600,2016,2,'odio donec vitae nisi nam ultrices libero non mattis pulvinar','True','False','False',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3389,986021214,2016,3,'ac est lacinia nisi venenatis tristique fusce','True','True','False',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3390,153845063,2016,1,'at lorem integer tincidunt ante','True','True','False',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3391,862374613,2016,2,'nisi at nibh in hac habitasse platea dictumst aliquam augue','True','True','False',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3392,481404914,2016,3,'convallis tortor risus dapibus augue','True','True','True',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3393,649505461,2016,1,'dignissim vestibulum vestibulum ante ipsum primis','False','True','True',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3394,296174458,2016,2,'ut massa volutpat convallis morbi odio odio','False','True','True',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3395,156384792,2016,3,'dictumst morbi vestibulum velit id pretium iaculis diam','False','True','True',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3396,439080320,2016,1,'ante ipsum primis in faucibus orci luctus et','False','False','True',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3397,495949997,2016,2,'donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin','False','False','True',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3398,284556821,2016,3,'gravida sem praesent id massa id nisl venenatis lacinia aenean','True','False','True',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3399,175912417,2016,1,'tellus semper interdum mauris ullamcorper purus sit','True','False','False',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3400,684857725,2016,2,'bibendum felis sed interdum venenatis turpis enim','True','False','False',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3401,614113042,2016,3,'maecenas pulvinar lobortis est phasellus sit','True','True','False',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3402,930247312,2016,1,'varius integer ac leo pellentesque ultrices mattis odio','True','True','False',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3403,318669979,2016,2,'eu nibh quisque id justo sit amet sapien dignissim','True','True','False',2229);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3404,990345780,2016,3,'lacinia nisi venenatis tristique fusce','True','True','True',2230);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3405,929187861,2016,1,'odio donec vitae nisi nam ultrices libero non mattis pulvinar','False','True','True',2222);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3406,198455366,2016,2,'ac est lacinia nisi venenatis tristique fusce','False','True','True',2223);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3407,614494816,2016,3,'at lorem integer tincidunt ante','False','True','True',2224);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3408,101866298,2016,1,'nisi at nibh in hac habitasse platea dictumst aliquam augue','False','False','True',2225);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3409,309935483,2016,2,'convallis tortor risus dapibus augue','False','False','True',2226);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3410,902778391,2016,3,'dignissim vestibulum vestibulum ante ipsum primis','True','False','True',2227);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3411,446901428,2016,1,'ut massa volutpat convallis morbi odio odio','True','False','False',2228);
+INSERT INTO MATA_KULIAH_SPESIAL VALUES (3412,199331206,2016,2,'dictumst morbi vestibulum velit id pretium iaculis diam','True','False','False',2229);
